b2255ad157284706f22e88471907a09c729e6182
Ahmed-abulfateh <ahmed-bh91@live.com>
2026-01-20 09:26:13 +0300

    product schema changed

--- models/Product.js
+++ models/Product.js
@@ -7,21 +7,23 @@ const productSchema = new mongoose.Schema({
   },
   description: {
     type: String,
+    required: true,
   },
   price: {
     type: Number,
     required: true,
   },
-  userId: {
+  stock: {
+    type: Number,
+    required: true,
+    default: 0,
+  },
+  createdBy: {
     type: mongoose.Schema.Types.ObjectId,
     ref: "User",
     required: true,
   },
-  createdAt: {
-    type: Date,
-    default: Date.now,
-  },
-});
+}, { timestamps: true });
 
 const Product = mongoose.model("Product", productSchema);
 
