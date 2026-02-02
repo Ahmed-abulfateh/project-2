0c3d1b23799dd744ccd549641d05adcdd8c200d6
Ahmed-abulfateh <ahmed-bh91@live.com>
2026-02-02 11:12:15 +0300

    Update is-ahmed middleware

--- middleware/is-ahmed.js
+++ middleware/is-ahmed.js
@@ -2,6 +2,6 @@ module.exports = (req, res, next) => {
   if (req.session.user && req.session.user.role === "admin" && req.session.user.username === "Ahmed") {
     next();
   } else {
-    res.send("Access Denied: Only Ahmed can manage users");
+    res.send("Access Denied: Only certain-Admin can manage users");
   }
 };
